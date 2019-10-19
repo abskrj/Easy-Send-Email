0d0b3164dd6e0d0db666bb90e27eb11e5e0d5f79
unknown <niangomes@gmail.com>
2019-10-19 16:07:59 -0300

    add attachment to email

--- test.py
+++ test.py
@@ -1,4 +1,48 @@
-import smtplib, ssl, time
+import ssl, time
+import mimetypes
+import os
+import smtplib
+
+from email import encoders
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+#this item add the attachment to email 
+
+def add_attachment(msg, filename):
+    if not os.path.isfile(filename):
+        return
+
+    ctype, encoding = mimetypes.guess_type(filename)
+
+    if ctype is None or encoding is not None:
+        ctype = 'application/octet-stream'
+
+    maintype, subtype = ctype.split('/', 1)
+
+    if maintype == 'text':
+        with open(filename) as f:
+            mime = MIMEText(f.read(), _subtype=subtype)
+    elif maintype == 'image':
+        with open(filename, 'rb') as f:
+            mime = MIMEImage(f.read(), _subtype=subtype)
+    elif maintype == 'audio':
+        with open(filename, 'rb') as f:
+            mime = MIMEAudio(f.read(), _subtype=subtype)
+    else:
+        with open(filename, 'rb') as f:
+            mime = MIMEBase(maintype, subtype)
+            mime.set_payload(f.read())
+
+        encoders.encode_base64(mime)
+
+    mime.add_header('Content-Disposition', 'attachment', filename=filename)
+    msg.attach(mime)
+
 '''
 This file takes the SMTP Sender Email address, Receiver Email address as inputs and 
 sends a mail with a specific message using GMail as the SMTP server.
